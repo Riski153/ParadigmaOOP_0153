142e52eb2501809cb16b1bfb8da11ab63bf5004d
Riski153 <m.riski.f21@mail.umy.ac.id>
2026-04-28 09:33:08 +0700

    menambahkan class mahasiswa

--- classMahasiswa.cpp
+++ classMahasiswa.cpp
@@ -2,3 +2,18 @@
 #include <string>
 
 using namespace std;
+
+class Mahasiswa
+{
+public: // akses modifier
+    int NIM;
+    string nama;
+    float nilai;
+
+    void printData() 
+    {
+        cout << "NIM   = " << NIM << endl;
+        cout << "Nama  = " << nama << endl;
+        cout << "Nilai = " << nilai << endl;
+    }
+};
